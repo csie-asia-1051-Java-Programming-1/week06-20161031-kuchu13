924e8772d9eda76cbb957767a1de170f5b2624a0
kuchu13 <105021033@live.asia.edu.tw>
2016-10-31 14:27:24 +0800

    1 OK

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,18 +1,34 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 寫一函數 square(n,c)，以 c 字元，印出邊長為 n 之實心正方形。Ex：square(3,'@') 將印出如下形狀
  @@@
  @@@
  @@@
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021033 黃苡珊
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入數字");
+		int n = scn.nextInt();
+		System.out.print("請輸入符號");
+		char c = scn.next().charAt(0);
+		square(n, c);
 	}
 
+	private static void square(int v1, char v2) {
+
+		for (int i = v1; i > 0; i--) {
+			for (int j = v1; j > 0; j--) {
+				System.out.print(v2);
+			}
+			System.out.println();
+		}
+	}
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,7 +2,7 @@
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021033 黃苡珊
  */
 
 public class ex02 {
