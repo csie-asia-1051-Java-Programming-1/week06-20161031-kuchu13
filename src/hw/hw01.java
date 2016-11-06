93560a35cee4599f322ea060ca3a2a7a976ab641
kuchu13 <105021033@live.asia.edu.tw>
2016-11-07 02:24:25 +0800

    hm

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,22 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		float v1 = scn.nextFloat();
+		int type = scn.nextInt();
+		System.out.println(fun1(v1, type));
 	}
-
+	public static float fun1(float a, int b) {
+		float i = 0;
+		if (b == 1) {
+			i = (a - 32) * 5 / 9;
+		} else if (b == 2) {
+			i = (a * 9 / 5) + 32;
+		}
+		return i;
+	}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,23 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner scn = new Scanner(System.in);
+	
+	int v1 = scn.nextInt();
+	float v2 = scn.nextFloat();
+	System.out.println(fun1(v1,v2));
+}
+public static float fun1(int a, float b) {
+	float i = 0;
+	if (a == 1) {
+		i = (float) ((b-170)*0.6+62);
+	} else if (a == 2) {
+		i = (float) ((b-158)*0.5+52);
 	}
-
+	return i;
+}
 }
